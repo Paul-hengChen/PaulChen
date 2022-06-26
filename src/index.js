e3d47f41c09db908a1ee7ff49d27096ef031e0a7
Paul-hengChen <fgvb88997@gmail.com>
2022-06-27 00:39:58 +0800

    chore: setting webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />,
+// 建立一個DOM物件
+const element = <h1>Hellsssssso, world!</h1>;
+
+// 使用ReactDOM.render把剛建立的物件element插入目標DOM中
+ReactDOM.render(
+  element,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-var */
 var path = require('path');
-var webpack = require('webpack');
+// var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -9,23 +9,22 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  // plugins: [
-  //   new webpack.optimize.UglifyJsPlugin({
-  //     compressor: {
-  //       warnings: false,
-  //     },
-  //   }),
-  //   new webpack.optimize.OccurrenceOrderPlugin(),
-  //   new HtmlWebpackPlugin({
-  //     template: './src/index.html',
-  //   }),
-  // ],
-  // module: {
-  //   loaders: [
-  //     {
-  //       test: /\.css$/,
-  //       loaders: ['style', 'css', 'sass'],
-  //     },
-  //   ],
-  // },
+  module: {
+    rules: [
+      // 第一個loader編譯JSX
+      { test: /.jsx$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-react'] } } },
+      // 第二個loader編譯ES6
+      { test: /.js$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env'] } } },
+    ],
+  },
+  devServer: {
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+      favicon: './public/favicon.ico',
+    }),
+  ],
 };
